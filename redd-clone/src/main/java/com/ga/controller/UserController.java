8fc4c7b2a112fc853e6cb54ae0f2fc9fbcb4dfd3
aridoza <mendozaarielr@gmail.com>
2019-10-21 16:33:10 -0400

    added user and userController

--- redd-clone/src/main/java/com/ga/controller/UserController.java
+++ redd-clone/src/main/java/com/ga/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.ga.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ga.entity.User;
 
 @RestController
 @RequestMapping("/user")
@@ -11,5 +16,15 @@ public class UserController {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World!!";
+	}
+	
+	@GetMapping("/users")
+	public List<User> getUsers() {
+		List<User> users = new ArrayList<User>();
+		
+		users.add(new User("batman", "bat", "batman@superhero.com"));
+		users.add(new User("spiderman", "spider", "spiderman@superhero.com"));
+		
+		return users;
 	}
 }
---
+++ redd-clone/src/main/java/com/ga/entity/User.java
@@ -0,0 +1,49 @@
+package com.ga.entity;
+
+public class User {
+	
+    private Long userId;
+    private String username;
+    private String password;
+    private String email;
+
+    public User() {}
+    
+    public User(String username, String password, String email) {
+    	this.username = username;
+    	this.password = password;
+    	this.email = email;
+    }
+
+    public Long getUserId() {
+	return userId;
+    }
+
+    public void setUserId(Long userId) {
+	this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getPassword() {
+	return password;
+    }
+
+    public void setPassword(String password) {
+	this.password = password;
+    }
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
